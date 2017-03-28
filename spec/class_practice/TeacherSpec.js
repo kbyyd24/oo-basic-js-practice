e9ad650c17f859cd0d3aa5524c4d598cf704c9ab
gaoyx <melo@gaoyuexiang.cn>
2017-03-28 17:52:44 +0800

    fixed test cases for Teacher.introduce

--- spec/class_practice/TeacherSpec.js
+++ spec/class_practice/TeacherSpec.js
@@ -1,5 +1,6 @@
 describe('teacher', function () {
   const Class = require('../../lib/class_practice/Class');
+  const Teacher = require('../../lib/class_practice/Teacher');
   it('should introduce class when given class', function () {
     const teacher = new Teacher('Tom', 21, new Class(2));
     expect(teacher.introduce())
