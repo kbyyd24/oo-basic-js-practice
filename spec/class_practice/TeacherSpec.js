c389e7d4d75e2f09b557c16fe4349cd224d7e955
gaoyx <melo@gaoyuexiang>
2017-03-28 17:59:21 +0800

    add test cases for Teacher.introduceWith

--- spec/class_practice/TeacherSpec.js
+++ spec/class_practice/TeacherSpec.js
@@ -11,4 +11,19 @@ describe('teacher', function () {
     expect(teacher.introduce())
       .toEqual('My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.');
   })
+
+  const Student = require('../../lib/class_practice/Person');
+  it('should introduce teaching student when given a student the teacher is teaching', function () {
+    const classObj = new Class(2);
+    const student = new Student('Jerry', 12, classObj);
+    const teacher = new Teacher('Tom', 21, classObj);
+    expect(teacher.introduceWith(student))
+      .toEqual('My name is Tom. I am 21 years old. I am a Teacher. I teach Jerry.');
+  });
+  it('should not introduce teaching student when given a student the teacher is not teaching', function () {
+    const student = new Student('Jerry', 12, new Class(1));
+    const teacher = new Teacher('Tom', 21, new Class(2));
+    expect(teacher.introduceWith(student))
+      .toEqual("My name is Tom. I am 21 years old. I am a Teacher. I don't teach Jerry.");
+  });
 });
