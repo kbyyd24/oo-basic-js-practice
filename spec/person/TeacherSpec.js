ad0d7a61ae7db8746e1c49f5da36cc1d4062639c
gaoyx <melo@gaoyuexiang.cn>
2017-03-28 17:15:17 +0800

    fixed test cases for teacher

--- spec/person/TeacherSpec.js
+++ spec/person/TeacherSpec.js
@@ -1,4 +1,5 @@
 describe('teacher', function () {
+  const Teacher = require('../../lib/person/Teacher');
   it('should introduce with teach class when given a classNum', function () {
     const teacher = new Teacher('Tom', 21, 2);
     expect(teacher.introduce()).toEqual('My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2.');
