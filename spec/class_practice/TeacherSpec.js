ed7ddb8cb2622466b8686798bc6c7cc5acfb5167
gaoyx <melo@gaoyuexiang.cn>
2017-03-28 21:46:50 +0800

    [refactor] update introduceWith

--- spec/class_practice/TeacherSpec.js
+++ spec/class_practice/TeacherSpec.js
@@ -1,12 +1,12 @@
 describe('teacher', function () {
   const Class = require('../../lib/class_practice/Class');
   const Teacher = require('../../lib/class_practice/Teacher');
-  it('should introduce class when given clazz', function () {
+  it('should introduce class when given class', function () {
     const teacher = new Teacher('Tom', 21, new Class(2));
     expect(teacher.introduce())
       .toEqual('My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2.');
   });
-  it('should not introduce class when given no clazz', function () {
+  it('should not introduce class when given no class', function () {
     const teacher = new Teacher('Tom', 21);
     expect(teacher.introduce())
       .toEqual('My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.');
