6ce5cdfdeeadfa0df56eb25e26dd08ca3f18de1a
gaoyx <melo@gaoyuexiang>
2017-03-28 11:50:37 +0800

    add test case for person

--- spec/person/personSpec.js
+++ spec/person/personSpec.js
@@ -1,8 +1,8 @@
 describe('person', function () {
   it('should use introduce method success when invoke person.introduce', function () {
-    const name = 'Melo';
+    const name = 'Bryant';
     const age = 22;
     const person = new Person(name, age);
-    expect(person.introduce()).toEqual('My name is Melo. I am 22 years old.')
+    expect(person.introduce()).toEqual('My name is Bryant. I am 22 years old.')
   });
 });
