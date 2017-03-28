d03d0d261b41c2340089503832ff4abddf57c167
gaoyx <melo@gaoyuexiang>
2017-03-28 17:48:31 +0800

    add test cases for Teacher.introduce

---
+++ spec/class_practice/TeacherSpec.js
@@ -0,0 +1,13 @@
+describe('teacher', function () {
+  const Class = require('../../lib/class_practice/Class');
+  it('should introduce class when given class', function () {
+    const teacher = new Teacher('Tom', 21, new Class(2));
+    expect(teacher.introduce())
+      .toEqual('My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2.');
+  });
+  it('should not introduce class when given no class', function () {
+    const teacher = new Teacher('Tom', 21);
+    expect(teacher.introduce())
+      .toEqual('My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.');
+  })
+});
