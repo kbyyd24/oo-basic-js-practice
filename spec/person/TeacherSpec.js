1f11cb22e7113df835b5d4ec357d7b53c5a8123d
gaoyx <melo@gaoyuexiang.cn>
2017-03-28 18:06:44 +0800

    rename class, classObj to clazz

--- spec/person/TeacherSpec.js
+++ spec/person/TeacherSpec.js
@@ -1,10 +1,10 @@
 describe('teacher', function () {
   const Teacher = require('../../lib/person/Teacher');
-  it('should introduce with teach class when given a classNum', function () {
+  it('should introduce with teach clazz when given a classNum', function () {
     const teacher = new Teacher('Tom', 21, 2);
     expect(teacher.introduce()).toEqual('My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2.');
   });
-  it('should introduce without teach class when given no classNum', function () {
+  it('should introduce without teach clazz when given no classNum', function () {
     const teacher = new Teacher('Tom', 21);
     expect(teacher.introduce()).toEqual('My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.');
   });
