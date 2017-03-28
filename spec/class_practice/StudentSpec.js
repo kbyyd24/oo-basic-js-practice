f2fb8d2cc55998e1f0f36eabedc994096123ab68
gaoyx <melo@gaoyuexiang>
2017-03-28 17:39:35 +0800

    add test case for Student

---
+++ spec/class_practice/StudentSpec.js
@@ -0,0 +1,8 @@
+describe('student', function () {
+  const Class = require('../../lib/class_practice/Class');
+  it('should get introduction success when invoke student.introduce', function () {
+    const student = new Student('Tom', 21, new Class(2));
+    expect(student.introduce())
+      .toEqual('My name is Tom. I am 21 years old. I am a Student. I am at Class 2.');
+  });
+});
