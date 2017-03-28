f47270a97cf2da173eae7cb6a18e98987e2ce73f
gaoyx <melo@gaoyuexiang>
2017-03-28 15:26:48 +0800

    fixed test cases

--- spec/person/WorkerSpec.js
+++ spec/person/WorkerSpec.js
@@ -1,8 +1,8 @@
 describe('worker', function () {
   const Worker = require('../../lib/person/Worker');
   it('should use introduce success when invoke worker.introduce', function () {
-    const name = '2333';
-    const age = 2333;
+    const name = 'Tom';
+    const age = 21;
     const worker = new Worker(name, age);
     expect(worker.introduce()).toEqual('My name is Tom. I am 21 years old. I am a Worker. I have a job.');
   });
