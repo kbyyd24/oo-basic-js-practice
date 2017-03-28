7b64632c1031cba13ca514892e8990655c0eefa6
gaoyx <melo@gaoyuexiang.cn>
2017-03-28 17:35:22 +0800

    fixed test case for person in class_practice

--- spec/class_practice/PersonSpec.js
+++ spec/class_practice/PersonSpec.js
@@ -1,4 +1,5 @@
 describe('person', function () {
+  const Person = require('../../lib/class_practice/Person');
   it('should get introduction success when invoke person.success', function () {
     const person = new Person('Tom', 21);
     expect(person.introduce()).toEqual('My name is Tom. I am 21 years old.');
